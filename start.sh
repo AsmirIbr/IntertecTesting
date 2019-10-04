4b26ef5a11eb49f584a5f6ea2d81d8fd811688d4
asmir <asmir@exodus.com.mk>
2019-10-04 14:35:05 +0200

    aq

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 exec > >(tee /var/log/start-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 cd /root/lottery
+forever stop dist/index.js
 rm -rf node_modules
 npm install
-npm run build
-service nodejs restart
+forever start dist/index.js
 echo "done"
