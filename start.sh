3a6bf266704648464c91a977833646b1335218ff
asmir <asmir@exodus.com.mk>
2019-10-04 14:00:57 +0200

    aaq

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 exec > >(tee /var/log/start-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-nohup npm run dev
+rm -rf node_modules
+npm install
+npm run build
+nohup npm run serve
