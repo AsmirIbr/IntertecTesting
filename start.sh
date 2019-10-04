316bdc40c2208f21324cc43818005a7652e2ddcb
asmir <asmir@exodus.com.mk>
2019-10-04 15:44:41 +0200

    rew

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 exec > >(tee /var/log/start-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 cd /root/lottery
-forever stop dist/index.js
 rm -rf node_modules
 npm install
-forever start dist/index.js
+npm run build
+npm run serve
 echo "done"
