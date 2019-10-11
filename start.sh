0acd133d7bb1fd194361f2a942c56a17c7506434
asmir <asmir@exodus.com.mk>
2019-10-11 22:00:47 +0200

    asdasd

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ exec > >(tee /var/log/start-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 cd /root/lottery
 rm -rf node_modules
 npm install
-service nodejs restart --force
+service nodejs stop --force
+service nodejs start --force
