e488554ec11920e4d4db751b6b9f4bb029c17679
asmir <asmir@exodus.com.mk>
2019-10-14 18:14:38 +0200

    asffa

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use('/', (req,res) => {
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-// API listener
+// API listener!
 app.listen(port, () => {
   console.log(`API is running on ${port}!`);
 });
