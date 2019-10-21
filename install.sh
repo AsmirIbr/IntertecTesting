5b2780b6fef3b1376eb847e133018fe606870f05
asmir <asmir@exodus.com.mk>
2019-10-21 16:50:40 +0200

    sdfsf

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 exec > >(tee /var/log/install-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-cd /var/dbConfig
-
 username=$(aws secretsmanager get-secret-value --secret-id dbConnection --query SecretString --region eu-west-2 --output text | echo $(jq -r .username))
 password=$(aws secretsmanager get-secret-value --secret-id dbConnection --query SecretString --region eu-west-2 --output text | echo $(jq -r .password))
 database=$(aws secretsmanager get-secret-value --secret-id dbConnection --query SecretString --region eu-west-2 --output text | echo $(jq -r .dbname))
@@ -10,9 +8,9 @@ host=$(aws secretsmanager get-secret-value --secret-id dbConnection --query Secr
 port=$(aws secretsmanager get-secret-value --secret-id dbConnection --query SecretString --region eu-west-2 --output text | echo $(jq -r .port))
 dialect=$(aws secretsmanager get-secret-value --secret-id dbConnection --query SecretString --region eu-west-2 --output text | echo $(jq -r .engine))
 
-echo $(jq '(.production | .username) = "'$username'"' mysql.json) > mysql.json
-echo $(jq '(.production | .password) = "'$password'"' mysql.json) > mysql.json
-echo $(jq '(.production | .database) = "'$database'"' mysql.json) > mysql.json
-echo $(jq '(.production | .host) = "'$host'"' mysql.json) > mysql.json
-echo $(jq '(.production | .port) = "'$port'"' mysql.json) > mysql.json
-echo $(jq '(.production | .dialect) = "'$dialect'"' mysql.json) > mysql.json
+echo $(jq '(.production | .username) = "'$username'"' mysql.json) > /var/dbConfig/mysql.json
+echo $(jq '(.production | .password) = "'$password'"' mysql.json) > /var/dbConfig/mysql.json
+echo $(jq '(.production | .database) = "'$database'"' mysql.json) > /var/dbConfig/mysql.json
+echo $(jq '(.production | .host) = "'$host'"' mysql.json) > /var/dbConfig/mysql.json
+echo $(jq '(.production | .port) = "'$port'"' mysql.json) > /var/dbConfig/mysql.json
+echo $(jq '(.production | .dialect) = "'$dialect'"' mysql.json) > /var/dbConfig/mysql.json
